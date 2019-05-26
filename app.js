7731887a7582c2a86e2f5cba94dc0cc8b0819a79
Ahmad Kamal <Ahmad106107@gmail.com>
2019-05-26 02:19:20 +0200

    done

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 var riddles = [
   {
+    riddle: ["The layer of the planet Earth where life exists"],
+    answer: "BIOSPHERE"
+  },
+  {
     riddle: [
-      "The thunder comes before the lightning,",
-      "And the lightning comes before the cloud,",
-      "The rain dries all the land it touches,",
-      "Wrapping the earth in a blood red shroud."
+      "It occurs when carbon dioxide gas (or CO2) is absorbed by the ocean"
     ],
-    answer: "VOLCANO"
+    answer: "ACIDIFICATION"
   },
 
   {
     riddle: [
-      "I can eat everything",
-      "but I can't drink anything",
-      "so tell me who I am"
+      "Sustainability framework where the design and production of materials,",
+      "structures, and systems",
+      "are modelled on biological entities and processes."
     ],
-    answer: "FIRE"
-  },
-
-  {
-    riddle: ["Something you have", "but the others use it more than you"],
-    answer: "NAME"
+    answer: "BIOMIMICRY"
   }
 ];
 
+// Grabbing HTML Interface Elements
 var startBtn = document.querySelector("#startBtn");
 var msgField = document.querySelector("#messageField");
 var gameFooter = document.querySelector(".gameFooter");
@@ -32,104 +29,130 @@ var wrongLetters = document.querySelector("#wrongGuesses");
 var winsCount = document.querySelector("#wins");
 var wins = 0;
 
+// Initiate Game start
 startBtn.onclick = function() {
   gameStart();
 };
 
+//Game start function
 function gameStart() {
+  // Initial properties at the start of each new game.
   var score = 0;
   var chances = 7;
+
   var rightGuess = [];
   var wrongGuess = [];
   lifeCount.innerHTML = chances + " attempts left";
   winsCount.innerHTML = "Wins: " + wins;
   wrongLetters.innerHTML = "Wrong Guesses: " + wrongGuess;
+  // Pick random riddle from the array of objects.
+  if (riddles.length > 0) {
+    var randRiddle = riddles[Math.floor(Math.random() * riddles.length)];
+    riddles.pop(randRiddle);
+    console.log(riddles);
+    // Assign the selected riddles answer to a variable.
+    var randAnswer = randRiddle.answer;
+    // Split each letter from the answer into a brand new array.
+    var letterArr = randRiddle.answer.split("");
+    console.log(randRiddle.answer);
+    console.log(letterArr);
+    // Create, Display, and Assign the guessing field in the interface. Get rid of start button.
+    gameFooter.innerHTML = '<div class="wordWrap"></div>';
+    var wordWrap = document.querySelector(".wordWrap");
+    // Create a blank underlined space with a unique ID for each letter in the answer.
+    for (var i = 0; i < letterArr.length; i++) {
+      var mask = document.createElement("span");
+      mask.className = "mask";
+      mask.id = "id" + i;
+      wordWrap.appendChild(mask);
+    }
+    // Display the riddle in the interface.
+    msgField.innerHTML = '<div class="riddleWrap"></div>';
+    var riddleWrap = document.querySelector(".riddleWrap");
+    // Display each line of the riddle picked from the array on its unique line.
+    for (var i = 0; i < randRiddle.riddle.length; i++) {
+      var line = document.createElement("li");
+      riddleWrap.appendChild(line);
+      line.innerHTML = randRiddle.riddle[i] + "</br>";
+    }
+    // Event watch for the users keyboard choices.
+    document.onkeyup = function(event) {
+      var keyInput = event.key.toUpperCase();
+      var letterIndex = letterArr.indexOf(keyInput);
+      var keyInputCode = keyInput.charCodeAt(0);
 
-  var randRiddle = riddles[Math.floor(Math.random() * riddles.length)];
+      //Check if the keys pressed are capital letters, and not meta keys.
+      if (keyInput.length == 1 && (keyInputCode >= 65 && keyInputCode <= 99)) {
+        // Check if the letter pressed is in the answer.
+        if (letterIndex !== -1) {
+          // Check if this is not a duplicate selection. Then execute code.
+          if (rightGuess.indexOf(keyInput) === -1) {
+            // Push new letters to rightGuess array to prevent future duplicates.
+            rightGuess.push(keyInput);
+            // Unmask each correct letter in its appropriate position.
+            for (var i = 0; i < randAnswer.length; i++) {
+              if (keyInput === randAnswer.charAt(i)) {
+                var unMask = document.getElementById("id" + i);
+                unMask.className = "unMask";
+                unMask.innerHTML = keyInput;
+                // Increase score in order to keep track of correct letters.
+                score++;
+              }
+              // If score matches the length of the answer, the player wins.
+              if (score === randAnswer.length) {
+                wrongGuess.length = 0;
+                wrongLetters.innerHTML = "";
+                var newBtn =
+                  "<div class='btnWrap'><button id='playAgain' class='btn' autofocus>PLAY AGAIN</button></div>";
+                riddleWrap.innerHTML = "<p>YOU WIN!</p></br>" + newBtn;
+                wrongLetters.innerHTML = "Wrong Guesses: ";
+                var playAgain = document.querySelector("#playAgain");
+                playAgain.onclick = function() {
+                  wins++;
 
-  var randAnswer = randRiddle.answer;
-
-  var letterArr = randRiddle.answer.split("");
-  console.log(randRiddle.answer);
-  console.log(letterArr);
-
-  gameFooter.innerHTML = '<div class="wordWrap"></div>';
-  var wordWrap = document.querySelector(".wordWrap");
-
-  for (var i = 0; i < letterArr.length; i++) {
-    var mask = document.createElement("span");
-    mask.className = "mask";
-    mask.id = "id" + i;
-    wordWrap.appendChild(mask);
-  }
-
-  msgField.innerHTML = '<div class="riddleWrap"></div>';
-  var riddleWrap = document.querySelector(".riddleWrap");
-
-  for (var i = 0; i < randRiddle.riddle.length; i++) {
-    var line = document.createElement("li");
-    riddleWrap.appendChild(line);
-    line.innerHTML = randRiddle.riddle[i] + "</br>";
-  }
-
-  document.onkeyup = function(event) {
-    var keyInput = event.key.toUpperCase();
-    var letterIndex = letterArr.indexOf(keyInput);
-    var keyInputCode = keyInput.charCodeAt(0);
-
-    if (keyInput.length == 1 && (keyInputCode >= 65 && keyInputCode <= 99)) {
-      if (letterIndex !== -1) {
-        if (rightGuess.indexOf(keyInput) === -1) {
-          rightGuess.push(keyInput);
-
-          for (var i = 0; i < randAnswer.length; i++) {
-            if (keyInput === randAnswer.charAt(i)) {
-              var unMask = document.getElementById("id" + i);
-              unMask.className = "unMask";
-              unMask.innerHTML = keyInput;
-
-              score++;
-            }
-
-            if (score === randAnswer.length) {
-              wrongGuess.length = 0;
-              wrongLetters.innerHTML = "";
-              var newBtn =
-                "<div class='btnWrap'><button id='playAgain' class='btn' autofocus>PLAY AGAIN</button></div>";
-              riddleWrap.innerHTML = "<p>YOU WIN!</p></br>" + newBtn;
-              wrongLetters.innerHTML = "Wrong Guesses: ";
-              var playAgain = document.querySelector("#playAgain");
-              playAgain.onclick = function() {
-                wins++;
-                gameStart();
-              };
+                  gameStart();
+                };
+              }
             }
           }
-        }
-      } else {
-        if (chances > 0 && wrongGuess.indexOf(keyInput) === -1) {
-          wrongGuess.push(keyInput);
+        } else {
+          // Prevents the "attempts left" number from going into negatives.
+          if (chances > 0 && wrongGuess.indexOf(keyInput) === -1) {
+            wrongGuess.push(keyInput);
+            // Decrease and the attempts left number and change the interface.
+            chances--;
+            lifeCount.innerHTML = chances + " attempts left";
+            wrongLetters.innerHTML = "Wrong Guesses: " + wrongGuess;
+          }
+          // If we reach 0 attempts left, the player loses.
+          if (chances == 0) {
+            var newBtn =
+              "<div class='btnWrap'><button id='tryAgain' class='btn' autofocus>TRY AGAIN</button></div>";
+            riddleWrap.innerHTML = "<p>YOU LOSE!</p></br>" + newBtn;
+            var tryAgain = document.querySelector("#tryAgain");
+            wrongGuess.length = 0;
+            wrongLetters.innerHTML = "Wrong Guesses: ";
+            document.onkeyup = function(event) {
+              console.log("you lost");
+              attempts--;
+            };
 
-          chances--;
-          lifeCount.innerHTML = chances + " attempts left";
-          wrongLetters.innerHTML = "Wrong Guesses: " + wrongGuess;
-        }
-
-        if (chances == 0) {
-          var newBtn =
-            "<div class='btnWrap'><button id='tryAgain' class='btn' autofocus>TRY AGAIN</button></div>";
-          riddleWrap.innerHTML = "<p>YOU LOSE!</p></br>" + newBtn;
-          var tryAgain = document.querySelector("#tryAgain");
-          wrongGuess.length = 0;
-          wrongLetters.innerHTML = "Wrong Guesses: ";
-          document.onkeyup = function(event) {
-            console.log("you lost");
-          };
-          tryAgain.onclick = function() {
-            gameStart();
-          };
+            tryAgain.onclick = function() {
+              gameStart();
+            };
+          }
         }
       }
-    }
+    };
+  } else {
+    msgField.innerHTML = '<div class="riddleWrap"></div>';
+    var riddleWrap = document.querySelector(".riddleWrap");
+    var newBtn1 =
+      "<div class='btnWrap'><button id='done' class='btn btn-danger' autofocus>Next</button></div>";
+    riddleWrap.innerHTML = "<p>Game Over</p></br>" + newBtn1;
+    var done = document.querySelector("#done");
+  }
+  done.onclick = function() {
+    console.log("hello");
   };
 }
